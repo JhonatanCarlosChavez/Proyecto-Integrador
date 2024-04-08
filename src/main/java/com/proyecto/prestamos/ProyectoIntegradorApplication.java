3957c782d845b1f567adc74dabdc795bd03a55e3
JhonatanCarlosChavez <i202122464@cibertec.edu.pe>
2024-04-08 15:00:15 -0500

    nuevo commit

---
+++ src/main/java/com/proyecto/prestamos/ProyectoIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.prestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/prestamos/ProyectoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.prestamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
